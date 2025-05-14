5b62952ddf33be674132feb2842986d423a2853f
anant_dixit_13 <anantdixit100@gmail.com>
2025-05-14 23:03:35 +0530

    controlleradded

---
+++ controllers/listing.js
@@ -0,0 +1,77 @@
+const Listing = require("../models/listing.js");
+
+module.exports.index = async (req, res) => {
+  const allListings = await Listing.find({});
+  //console.log("yes");
+  res.render("listings/index.ejs", { allListings });
+};
+
+module.exports.renderNewForm = (req, res) => {
+  console.log("yes");
+  res.render("listings/newlisting.ejs");
+};
+
+module.exports.showlistingdata = async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id)
+    .populate({ path: "reviews", populate: { path: "author" } })
+    .populate("owner");
+  if (!listing) {
+    req.flash("error", "Listing doesnot exist");
+    res.redirect("/listings");
+  }
+  res.render("listings/show.ejs", { listing });
+};
+
+module.exports.createListing = async (req, res, next) => {
+  let { title, description, image, price, location, country } = req.body;
+
+  const listing = new Listing({
+    title,
+    description,
+    image,
+    price,
+    location,
+    country,
+  });
+  listing.owner = req.user._id;
+  await listing.save();
+
+  req.flash("success", "New Listing Created");
+  res.redirect("/listings");
+};
+
+module.exports.renderEditForm = async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  if (!listing) {
+    req.flash("error", "Listing doesnot exist");
+    res.redirect("/listings");
+  }
+  res.render("listings/edit_listing.ejs", { listing });
+};
+
+module.exports.updateListing = async (req, res) => {
+  // console.log("put request recieve");
+  // res.send("working");
+  let { id } = req.params;
+  let { title, description, image, price, location, country } = req.body;
+
+  await Listing.findByIdAndUpdate(id, {
+    title,
+    description,
+    image,
+    price,
+    location,
+    country,
+  });
+  req.flash("success", "listing Updated!");
+  res.redirect(`/listings/${id}`);
+};
+
+module.exports.destoryListing = async (req, res) => {
+  let { id } = req.params;
+  await Listing.findByIdAndDelete(id);
+  req.flash("success", "Listing Deleted!");
+  res.redirect("/listings");
+};
--- routes/listing.js
+++ routes/listing.js
@@ -5,90 +5,27 @@ const ExpressError = require("../utils/ExpressError.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner } = require("../middleware.js");
 
-router.get("/", async (req, res) => {
-  const allListings = await Listing.find({});
-  //console.log("yes");
-  res.render("listings/index.ejs", { allListings });
-});
+const listingController = require("../controllers/listing.js");
+
+router.get("/", listingController.index);
 
 //create New listing
-router.get("/new", isLoggedIn, (req, res) => {
-  console.log("yes");
-  res.render("listings/newlisting.ejs");
-});
+router.get("/new", isLoggedIn, listingController.renderNewForm);
 
 //showing all data of particular property
 
-router.get("/:id", async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id)
-    .populate({ path: "reviews", populate: { path: "author" } })
-    .populate("owner");
-  if (!listing) {
-    req.flash("error", "Listing doesnot exist");
-    res.redirect("/listings");
-  }
-  res.render("listings/show.ejs", { listing });
-});
+router.get("/:id", listingController.showlistingdata);
 
 //now after taking input from form we add that new listing in database and redirect
-router.post(
-  "/",
-  wrapAsync(async (req, res, next) => {
-    let { title, description, image, price, location, country } = req.body;
-
-    const listing = new Listing({
-      title,
-      description,
-      image,
-      price,
-      location,
-      country,
-    });
-    listing.owner = req.user._id;
-    await listing.save();
-
-    req.flash("success", "New Listing Created");
-    res.redirect("/listings");
-  })
-);
+router.post("/", wrapAsync(listingController.createListing));
 
 //edit the data ,first we give get request to render form and then put request to do actual change in db
-router.get("/:id/edit", isLoggedIn, isOwner, async (req, res) => {
-  let { id } = req.params;
-  const listing = await Listing.findById(id);
-  if (!listing) {
-    req.flash("error", "Listing doesnot exist");
-    res.redirect("/listings");
-  }
-  res.render("listings/edit_listing.ejs", { listing });
-});
+router.get("/:id/edit", isLoggedIn, isOwner, listingController.renderEditForm);
 
 //update in db finally
-router.put("/:id", isLoggedIn, isOwner, async (req, res) => {
-  // console.log("put request recieve");
-  // res.send("working");
-  let { id } = req.params;
-  let { title, description, image, price, location, country } = req.body;
-
-  await Listing.findByIdAndUpdate(id, {
-    title,
-    description,
-    image,
-    price,
-    location,
-    country,
-  });
-  req.flash("success", "listing Updated!");
-  res.redirect(`/listings/${id}`);
-});
+router.put("/:id", isLoggedIn, isOwner, listingController.updateListing);
 
 //delete the listing
-router.delete("/:id", isLoggedIn, isOwner, async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndDelete(id);
-  req.flash("success", "Listing Deleted!");
-  res.redirect("/listings");
-});
+router.delete("/:id", isLoggedIn, isOwner, listingController.destoryListing);
 
 module.exports = router;
