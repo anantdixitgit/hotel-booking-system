01c233c74e9435bfff4e21c6fc07f37bc070afd0
anant_dixit_13 <anantdixit100@gmail.com>
2025-05-19 23:55:31 +0530

    feature loaded

--- controllers/listing.js
+++ controllers/listing.js
@@ -1,3 +1,9 @@
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapToken = process.env.MAP_TOKEN;
+
+//console.log("maptoken is", mapToken);
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
+
 const Listing = require("../models/listing.js");
 
 module.exports.index = async (req, res) => {
@@ -24,6 +30,14 @@ module.exports.showlistingdata = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
+  let response = await geocodingClient
+    .forwardGeocode({
+      query: req.body.location,
+      limit: 1,
+    })
+    .send();
+  // console.log(response.body.features[0].geometry);
+  // res.send("done");
   let url = req.file.path;
   let filename = req.file.filename;
   console.log(url, "..", filename);
@@ -39,8 +53,9 @@ module.exports.createListing = async (req, res, next) => {
   });
   listing.image = { url, filename };
   listing.owner = req.user._id;
-  await listing.save();
-
+  listing.geometry = response.body.features[0].geometry;
+  let savedListing = await listing.save();
+  console.log(savedListing);
   req.flash("success", "New Listing Created");
   res.redirect("/listings");
 };
--- models/listing.js
+++ models/listing.js
@@ -27,6 +27,17 @@ const listingSchema = new mongoose.Schema({
     type: Schema.Types.ObjectId,
     ref: "User",
   },
+  geometry: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
 });
 
 listingSchema.post("findOneAndDelete", async (listing) => {
